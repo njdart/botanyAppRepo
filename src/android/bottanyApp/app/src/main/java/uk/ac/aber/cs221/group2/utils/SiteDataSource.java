283a044758450eab7e68c618f0cebe8213d7c3e4
acb122 <acb12@aber.ac.uk>
2015-01-21 15:54:17 +0000

    read

--- src/android/bottanyApp/app/src/main/java/uk/ac/aber/cs221/group2/utils/SiteDataSource.java
+++ src/android/bottanyApp/app/src/main/java/uk/ac/aber/cs221/group2/utils/SiteDataSource.java
@@ -2,8 +2,12 @@ package uk.ac.aber.cs221.group2.utils;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -13,6 +17,11 @@ public class SiteDataSource {
 
     SQLiteOpenHelper dbhelper;
     SQLiteDatabase database;
+
+    private static final String[] allColumns = {
+            DatabaseUtils.COLUMN_ID,
+            DatabaseUtils.COLUMN_VISIT_NAME
+    };
     public SiteDataSource( Context context){
         dbhelper = new DatabaseUtils(context);
         database = dbhelper.getWritableDatabase();
@@ -33,5 +42,21 @@ public class SiteDataSource {
 
 
     }
+
+    public List<String> findAll(){
+        List<String>  sites =  new ArrayList<String>();
+        Cursor cursor = database.query(DatabaseUtils.TABLE_TOURS, allColumns ,null,null,null,null,null);
+        if(cursor.getCount()>0){
+            while(cursor.moveToNext()){
+                String site = new String();
+                site = cursor.getString(cursor.getColumnIndex(DatabaseUtils.COLUMN_VISIT_NAME));
+                sites.add(site);
+            }
+        }
+        return sites;
+
+    }
+
+
 
 }
