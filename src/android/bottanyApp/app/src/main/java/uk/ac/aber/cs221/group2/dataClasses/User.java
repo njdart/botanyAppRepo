2a342d8f651628c321dc78fbc6fd50e36f987aa4
wia3 <anw46@aber.ac.uk>
2015-01-22 10:57:41 +0000

    Uploaded data classes for java

--- src/android/bottanyApp/app/src/main/java/uk/ac/aber/cs221/group2/dataClasses/User.java
+++ src/android/bottanyApp/app/src/main/java/uk/ac/aber/cs221/group2/dataClasses/User.java
@@ -1,24 +1,77 @@
-package uk.ac.aber.cs221.group2.dataClasses;
+
+public class user {
+	
+	
+	private String ForeName;
+	private String LastName;
+	private String PhoneNumber;
+	private String Email;
+	
+	
+	
+	
+	/**
+	 * sets the users forename
+	 */
+	public void setForeName(String a){
+		ForeName = a;
+	}
+	
+	/**
+	 * sets the users lastname
+	 */
+	public void setLastName(String a){
+		LastName = a;
+	}
+	
+	/**
+	 * sets the users phonenumber
+	 */
+	public void setPhoneNumber(String a){
+		LastName = a;
+	}
+	
+	/**
+	 * sets the users email
+	 */
+	public void setEmail(String a){
+		LastName = a;
+	}
 
 
-public class User {
+	 /**
+     * Returns the users first name
+     */
+    public String getUserForeName() {
+		return ForeName;
+	}
 
-    private String name;
-    private String phoneNumber;
-    private String email;
-    private String siteLocation;
+    /**
+     * Returns the users last name
+     */
+    public String getUserLastName() {
+		return LastName;
+	}
 
-    //Possible use?
-    private double lat;
-    private double lng;
+    /**
+     * Returns the users phone number
+     * Formatted accordingly (Eg 07...)
+     */
+    public String getUserPhoneNumber() {
+		return PhoneNumber;
+	}
 
-    public User(String name,
-                String phone,
-                String email,
-                String siteLoc){
-        this.name = name;
-        this.phoneNumber = phone;
-        this.email = email;
-        this.siteLocation = siteLoc;
-    }
+    /**
+     * Returns the users email
+     */
+    public String getUserEmail() {
+		return Email;
+	}
+    
+    
+    
+    
+    
+    
+    
 }
