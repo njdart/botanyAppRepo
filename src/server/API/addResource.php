0d28392845cc440978a101b84c424ca34c029b10
maxxeh1 <max.atkins@live.com>
2015-01-26 21:26:56 +0000

    Fixed addResource

--- src/server/API/addResource.php
+++ src/server/API/addResource.php
@@ -7,7 +7,6 @@
 	
 	//Decodes the JSON into PHP readable from POST
 	$resource = $_FILES["resource"];
-	echo $resource;
 	if($resource["error"] > 0) {
         http_response_code(500);
         die("File error code " . $resource["error"]);
@@ -40,7 +39,6 @@
 	$uploadPath = './'. $CONFIG['resourcePath'] . $resourceID . ".jpg";
 	//Move file from temporary location to defined storage; error code 500 if can't
 	if(!move_uploaded_file($resource['tmp_name'], $uploadPath)) {
-		echo $uploadPath;
 		http_response_code(500);
 		die("Can not copy file");
 	}
